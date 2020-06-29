2de9aef3eb748fa30c68358ab3249884cb6bf51f
Ramón Mayedo Morales <55454008+ramonmayedo@users.noreply.github.com>
2020-06-29 08:54:06 -0600

    Mod

--- src/isokernel/x86/readDisk.c
+++ src/isokernel/x86/readDisk.c
@@ -20,16 +20,11 @@ int installDevices();
 
 int _main(int deviceId) {
 
-  /*  char *ptrD = (char*) 0xB8000;
-    char nuevo;
-    nuevo = ((deviceId & 0xF0) >> 4) + 0x30;
-    ptrD[0] = nuevo;
-    nuevo = (deviceId & 0x0F) + 0x30;
-    ptrD[2] = nuevo;*/
     installDevices();
-        while(1);
-    
+    while (1);
+
 }
+
 void outb(u32 addr, u8 value) {
     asm("movb %0,%%al" : "=m"(value));
     asm("mov %0,%%dx" : "=m"(addr));
@@ -50,17 +45,17 @@ u8 inb(u32 addr) {
     return value;
 }
 
-u16 inw(u32 addr){
-  u16 value; 
-  asm("mov %0,%%dx" :"=m"(addr));   
-  asm("inw %dx, %ax");
-  asm("movw %%ax,%0" :"=m"(value)); 
-  return value;   
+u16 inw(u32 addr) {
+    u16 value;
+    asm("mov %0,%%dx" : "=m"(addr));
+    asm("inw %dx, %ax");
+    asm("movw %%ax,%0" : "=m"(value));
+    return value;
 }
 
 int waitDisk(SdevicePort *device) {
-    while (inb(device->regControl) & 0x80);  
-   return 0;
+    while (inb(device->regControl) & 0x80);
+    return 0;
 }
 
 void initCommand(SdevicePort *device, u8 driveIndex) {
@@ -68,42 +63,39 @@ void initCommand(SdevicePort *device, u8 driveIndex) {
     u8 mask = (u8) (driveIndex & (1 << 4));
     outb(device->regBase + 0x6, mask);
     delay400ns(device);
-    outb(device->regBase + 0x1, 0x0);
-    outb(device->regBase + 0x4, (u8) (2048 & 0xFF));
-    outb(device->regBase + 0x5, (u8) (2048 >> 8));
+    outb(device->regBase + 0x1, 0x0);                //0 = PIO mode  DMA = 1
+    outb(device->regBase + 0x4, (u8) (2048 & 0xFF)); //Maxima cantiad de byte parte baja
+    outb(device->regBase + 0x5, (u8) (2048 >> 8));   //Maxima cantiad de byte parte alta
 }
 
 int readSector(SdevicePort *device, u8 driveIndex, u32 ablock, u32 asectorCount, char *abuffer) {
-    u8 readCmd[12] = {0xA8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    u8 readCmd[12] = {0xA8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; // 0xA8 = comando de lectura
     initCommand(device, driveIndex);
     outb(device->regBase + 0x7, 0xA0);
     int status;
     while ((status = inb(device->regBase + 0x7))&0x80);
-    
+
     while (!((status = inb(device->regBase + 0x7))&0x08)&&!(status & 0x1));
-    
-    if (status & 0x1){
-       char *ptrD = (char*) 0xB8000;
-      ptrD[1] = 0x39;
+
+    if (status & 0x1) {
+        return 0;
     }
-     
+
     readCmd[9] = 0x1; //Cantidad de Sectores
-    readCmd[2] = u8((ablock >> 24) & 0xFF); //Byte mas significativo LBA
-    readCmd[3] = u8((ablock >> 16) & 0xFF);
-    readCmd[4] = u8((ablock >> 8) & 0xFF);
-    readCmd[5] = u8(ablock & 0xFF); //Byte menos significativo LBA
+    readCmd[2] = u8((ablock >> 0x18) & 0xFF); //Byte mas significativo LBA
+    readCmd[3] = u8((ablock >> 0x10) & 0xFF);
+    readCmd[4] = u8((ablock >> 0x08) & 0xFF);
+    readCmd[5] = u8((ablock >> 0x00) & 0xFF); //Byte menos significativo LBA
 
     u16 *val = (u16*) & readCmd;
-    for (int i = 0; i < 6; i++) outw(device->regBase, val[i]);
+    for (int i = 0; i < 6; i++) outw(device->regBase, val[i]); //Se envia el comando de lectura
 
     int size = (int(inb(device->regBase + 0x5) << 8) | int(inb(device->regBase + 0x4)));
-    
-   if (size != 2048){
-       char *ptrD = (char*) 0xB8000;
-      ptrD[1] = 0x39;
-      return 0;
-   }
-       
+
+    if (size != 2048) {
+        return 0;
+    }
+
 
     u16 auxWord;
     for (int i = 0; i < size / 2; i++) {
@@ -111,10 +103,10 @@ int readSector(SdevicePort *device, u8 driveIndex, u32 ablock, u32 asectorCount,
         abuffer[i * 2] = (u8) auxWord;
         abuffer[i * 2 + 1] = (u8) (auxWord >> 8);
     }
-    
-    while (inb(device->regBase + 0x7)& 0x88) 
-        
-    return asectorCount;
+
+    while (inb(device->regBase + 0x7)& 0x88)
+
+        return asectorCount;
 }
 
 int installDevices() {
@@ -125,29 +117,22 @@ int installDevices() {
     chanel0.regControl = 0x3F6;
 
     char *buffer = (char*) 0x100000;
-    //for (int i = 29; i < 58; i++) {
-    readSector(&chanel1, 0, 29, 1, buffer);
-    //   buffer += 2048;
-    // }
-    char *ptrD = (char*) 0xB8000;
-    char val = *(char*) 0x100000;
-    val = (val & 0x0f);
-    char nuevo;
-    nuevo = val + 0x30;
-    ptrD[0] = nuevo;
-    while (1);
+    for (int i = 29; i < 29 + 56; i++) { //A partir del sector 29 esta la entrada al kernel en el iso
+        readSector(&chanel1, 0, i, 1, buffer);
+        buffer += 2048;
+    }
+
     asm("mov $0x000FFF0,%esp");
     asm("ljmp $0x18,$0x100000");
 }
 
-
 void resetBus(SdevicePort *device) {
-    outb(device->regControl, 0x04);   
-    for (int i = 0; i < 4; i++)  inb(device->regControl); //400ns   
+    outb(device->regControl, 0x04);
+    for (int i = 0; i < 4; i++) inb(device->regControl); //400ns   
     outb(device->regControl, 0x0);
 }
 
 void delay400ns(SdevicePort *device) {
     for (int i = 0; i < 4; i++)
-      inb(device->regControl); //400ns  
+        inb(device->regControl); //400ns  
 }
